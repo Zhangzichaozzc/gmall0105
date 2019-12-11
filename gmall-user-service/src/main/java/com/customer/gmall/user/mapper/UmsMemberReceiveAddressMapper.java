3c17968d86f108e312819880fc82b496fadab9d3
Zichao.zhang <zhangzc_zzc@163.com>
2019-12-11 19:50:18 +0800

    add gmall-user-web & gmall-user-service

---
+++ gmall-user-service/src/main/java/com/customer/gmall/user/mapper/UmsMemberReceiveAddressMapper.java
@@ -0,0 +1,14 @@
+package com.customer.gmall.user.mapper;
+
+import com.customer.gmall.bean.UmsMemberReceiveAddress;
+
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * UmsMemberReceiveAddressMapper
+ *
+ * @author Zichao Zhang
+ * @date 2019/12/10
+ */
+public interface UmsMemberReceiveAddressMapper extends Mapper<UmsMemberReceiveAddress> {
+}
---
+++ gmall-user-service/src/main/java/com/customer/gmall/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,48 @@
+package com.customer.gmall.user.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.customer.gmall.bean.UmsMember;
+import com.customer.gmall.bean.UmsMemberReceiveAddress;
+import com.customer.gmall.service.UserService;
+import com.customer.gmall.user.mapper.UmsMemberReceiveAddressMapper;
+import com.customer.gmall.user.mapper.UserMapper;
+
+import tk.mybatis.mapper.entity.Example;
+
+/**
+ * UserServiceImpl
+ *
+ * @author Zichao Zhang
+ * @date 2019/12/10
+ */
+//@Service
+@com.alibaba.dubbo.config.annotation.Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private UmsMemberReceiveAddressMapper umsMemberReceiveAddressMapper;
+
+
+    @Override
+    public List<UmsMember> getAllUser() {
+//        return userMapper.selectAllUser();
+        return userMapper.selectAll();
+    }
+
+    @Override
+    public List<UmsMemberReceiveAddress> getReceiveAddresseByMemberId(String memberId) {
+//        UmsMemberReceiveAddress umsMemberReceiveAddress = new UmsMemberReceiveAddress();
+//        umsMemberReceiveAddress.setMemberId(memberId);
+//        return umsMemberReceiveAddressMapper.select(umsMemberReceiveAddress);
+        Example example = new Example(UmsMemberReceiveAddress.class);
+        example.createCriteria().andEqualTo("memberId", memberId);
+        return umsMemberReceiveAddressMapper.selectByExample(example);
+    }
+}
